99565a6f36de3da9dfe1f7b62904c266a9dd4d65
Adam Lepley <Alepley@wheels.com>
2016-10-26 22:15:44 -0500

    test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var server = restify.createServer();
 
 // Setup Restify client
 var client = restify.createJsonClient({
-    url: 'http://machinelearningapi.azurewebsites.net'
+    url: 'https://machinelearningapi.azurewebsites.net'
 });
 
 // Create bot
