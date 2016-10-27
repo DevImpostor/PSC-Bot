eae7b4af98190ac0391c3e9b20a76861ad5ff4d8
Adam Lepley <Alepley@wheels.com>
2016-10-26 21:22:20 -0500

    Push

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
 
-var getProducts = function(searchText) {
+var getProducts = new function(searchText) {
     client.get('api/product/search/'+ searchText, function(err, req, res, obj) {
         assert.ifError(err);
         return obj;
