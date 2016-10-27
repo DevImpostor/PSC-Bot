360e2b7236ee20413a02a2543ff2361cbe6d2643
Adam Lepley <Alepley@wheels.com>
2016-10-26 21:24:44 -0500

    Test

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ bot.add('/', function (session) {
     
     //respond with user's message
     //session.send("You really said " + session.message.text);
-    session.send(JSON.stringify(getProducts("Lager")));
+    session.send(JSON.stringify(client));
 
 });
 
@@ -47,7 +47,7 @@ server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
 
-var getProducts = new function(searchText) {
+var getProducts = function(searchText) {
     client.get('api/product/search/'+ searchText, function(err, req, res, obj) {
         assert.ifError(err);
         return obj;
